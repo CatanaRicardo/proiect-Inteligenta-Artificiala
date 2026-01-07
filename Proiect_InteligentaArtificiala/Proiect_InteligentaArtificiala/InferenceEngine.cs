309c6b5f61897087457ddd2349f379ecca0b4488
ErhanRobert <robert-gabriel.erhan@student.tuiasi.ro>
2026-01-07 20:54:09 +0200

    Adaugare motor de inferenta si program principal

--- Proiect_InteligentaArtificiala/Proiect_InteligentaArtificiala/InferenceEngine.cs
+++ Proiect_InteligentaArtificiala/Proiect_InteligentaArtificiala/InferenceEngine.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Proiect_InteligentaArtificiala
+﻿// Motor de inferenta cu unificare si forward chaining
+class InferenceEngine
 {
-    class InferenceEngine
+    public void ForwardChaining(KnowledgeBase kb)
     {
+        bool factNou;
+
+        do{
+            factNou = false;
+
+            foreach (var rule in kb.Rules){
+                var substitutions = new Dictionary<string, string>();
+                bool toatePotrivite = true;
+
+                foreach (var premise in rule.Premises){
+                    bool gasit = false;
+
+                    foreach (var fact in kb.Facts){
+                        var tempSub = new Dictionary<string, string>(substitutions);
+
+                        if (premise.CanUnify(fact, tempSub)){
+                            substitutions = tempSub;
+                            gasit = true;
+                            break;
+                        }
+                    }
+
+                    if (!gasit){
+                        toatePotrivite = false;
+                        break;
+                    }
+                }
+
+                if (toatePotrivite){
+                    var concluzie = rule.Conclusion.ApplySubstitution(substitutions);
+
+                    if (!kb.Facts.Any(f => f.ToString() == concluzie.ToString())){
+                        Console.WriteLine("Se aplica regula:");
+                        Console.WriteLine("  " + rule);
+                        Console.WriteLine("Substitutii:");
+                        foreach (var s in substitutions)
+                            Console.WriteLine($"  {s.Key} = {s.Value}");
+                        Console.WriteLine("Rezulta concluzia:");
+                        Console.WriteLine("  " + concluzie);
+                        Console.WriteLine();
+
+                        kb.AddFact(concluzie);
+                        factNou = true;
+                    }
+                }
+            }
+        }
+        while (factNou);
     }
 }
--- Proiect_InteligentaArtificiala/Proiect_InteligentaArtificiala/Program.cs
+++ Proiect_InteligentaArtificiala/Proiect_InteligentaArtificiala/Program.cs
@@ -1,1 +1,393 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program{
+
+    static void Main(){
+        Console.WriteLine("=== PROIECT IA ===");
+        Console.WriteLine("Sistem de inferenta predicativa cu unificare\n");
+        DemonstratieNumarPar();
+        DemonstratieTriunghi();
+        Console.ReadKey();
+    }
+
+    static void DemonstratieNumarPar(){
+        Console.WriteLine("Teorema 1: Un numar par are patratul par\n");
+
+        KnowledgeBase kb = new KnowledgeBase();
+
+        Console.WriteLine("Premise initiale:");
+        kb.AddFact(new Predicate("Par", "n"));
+        kb.AddFact(new Predicate("Patrat", "n", "n2"));
+        Console.WriteLine(" - Par(n)");
+        Console.WriteLine(" - Patrat(n, n2)\n");
+
+        kb.Rules.Add(new Rule(
+            new List<Predicate>
+            {
+                new Predicate("Par", "?X"),
+                new Predicate("Patrat", "?X", "?Y")
+            },
+            new Predicate("Par", "?Y")
+        ));
+
+        new InferenceEngine().ForwardChaining(kb);
+
+        kb.PrintFacts();
+        Console.WriteLine("Concluzie: Patratul unui numar par este par.\n");
+    }
+
+    static void DemonstratieTriunghi(){
+        Console.WriteLine("Teorema 2: Un triunghi echilateral este isoscel\n");
+
+        KnowledgeBase kb = new KnowledgeBase();
+
+        Console.WriteLine("Premise initiale:");
+        kb.AddFact(new Predicate("Echilateral", "t"));
+        Console.WriteLine(" - Echilateral(t)\n");
+
+        kb.Rules.Add(new Rule(
+            new List<Predicate>
+            {
+                new Predicate("Echilateral", "?T")
+            },
+            new Predicate("Isoscel", "?T")
+        ));
+
+        new InferenceEngine().ForwardChaining(kb);
+
+        kb.PrintFacts();
+        Console.WriteLine("Concluzie: Triunghiul echilateral este isoscel.\n");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
